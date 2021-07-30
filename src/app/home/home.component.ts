d7df6853c5f24861ea80ecb4d4f4af2279ce6af3
DEEPAK KUMAR <deepakindradeva@gmail.com>
2021-07-31 01:48:35 +0530

    Initial Home Screen

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  WORD_SLIDER_TIMEOUT = 2e3;
+  triggerHeroWordSlide = () => {
+    setInterval(() => {
+      let current = document.querySelector('span[data-show]'),
+        other =
+          current?.nextElementSibling ||
+          document.querySelector('span:first-child'),
+        next = document.querySelector('span[data-up]');
 
-  constructor() { }
+      next && next.removeAttribute('data-up');
+      if (current) {
+        current.removeAttribute('data-show');
+        current.setAttribute('data-up', '');
+      }
 
-  ngOnInit(): void {
-  }
+      if (other) {
+        other.setAttribute('data-show', '');
+      }
+    }, this.WORD_SLIDER_TIMEOUT);
+  };
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
